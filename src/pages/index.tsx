f9bccdaa8c400345b13e278f3fe1c656ae864263
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-08-07 16:30:31 -0300

    feat: add login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
+import { useState, FormEvent } from 'react';
+
 function Home() {
-  return <h1>Next Auth</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = { email, password };
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+
+      <input
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <button type="submit">Entrar</button>
+    </form>
+  );
 }
 
 export default Home;
